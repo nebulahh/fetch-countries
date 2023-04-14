1aa4c94e2e8c6898979fd31c248b2169f380447b
orion <olalekancrown11@gmail.com>
2023-04-14 03:48:10 +0100

    setup homepage

--- src/pages/[countryId].tsx
+++ src/pages/[countryId].tsx
@@ -1,7 +1,12 @@
+import Navbar from '@/components/Navbar'
+import Link from 'next/link'
+import styles from '@/styles/Home.module.css'
+
 export default function Details({ details }) {
   console.log(details)
   return (
     <>
+    <Navbar />
 {
  details[0].name.official
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,27 +3,62 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Navbar from '@/components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home({ countries }) {
+
   return (
     <>
+    <Navbar />
+<main>
+<div className={styles.searchContainer}>
+  <div className={styles.searchBar}>
+<button className={styles.searchQuerySubmit} type="submit" name="searchQuerySubmit">
+      <svg
+      style={{width:24 +"px", height:24+"px" }}viewBox="0 0 24 24"><path fill="#666666" d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z" />
+      </svg>
+    </button>
+   
+    <input className={styles.searchQueryInput} type="text" name="searchQueryInput" placeholder="Search" value="" />
     
-<h1>List of coubtries</h1>
+  </div>
+  
+  
+<select name="choice">
+  <option value="first">First Value</option>
+  <option value="second" selected>Second Value</option>
+  <option value="third">Third Value</option>
+</select>
+
+</div>
+
+
 
 {
   countries.map((country, index)=> {
     return (
       <div key={index}>
 <Link href={`/${country.name.common}`} passHref>
-        <p>{country.name.official}, while capital - {country.capital}</p>
+
+<div className={styles.card}>
+  <img src={country.flags.png} alt=""/>
+  <div className={styles.description}>
+    <h2>{country.name.official}</h2>
+    <p>Population: {country.population}</p>
+    <p>Region: {country.region}</p>
+    <p>Capital: {country.capital}</p>
+    
+  </div>
+</div>
 </Link>
         <hr />
       </div>
       )
   })
 }
+</main>
     </>
   )
 }
