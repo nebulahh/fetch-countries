7b62b3a7fc9441ed6e8ac0acd1cd09ba345637cb
nebulahh <94326584+nebulahh@users.noreply.github.com>
2023-04-17 08:45:21 +0100

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -94,11 +94,11 @@ else if (filterParam == "All") {
 
 export async function getStaticProps() {
   const response = await fetch('https://restcountries.com/v3.1/all')
-  let countries = await response.json().slice(0, 50)
+  let countries = await response.json()
 
   return {
     props: {
-      countries
+      countries.slice(0, 50)
     }
   }
 }
